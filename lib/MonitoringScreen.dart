eec533ecbb375342a6aa2a4ec9ccc50464d337f4
Stanislaw <mikolajczykstanislaw415@gmail.com>
2024-11-29 21:43:12 +0100

    change bgc to white in body

--- lib/MonitoringScreen.dart
+++ lib/MonitoringScreen.dart
@@ -34,111 +34,114 @@ class _MonitoringScreenState extends State<MonitoringScreen> {
         ),
         automaticallyImplyLeading: false, // Usunięcie domyślnej ikony powrotu
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 20),
-              Text(
-                'Widok danych w czasie rzeczywistym:',
-                style: GoogleFonts.lato(
-                  textStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              // Placeholder for real-time data display
-              Card(
-                color: Colors.white,
-                elevation: 4,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('Temperatura wody'),
-                        subtitle: Text('22.5°C'), // Replace with real-time data
-                        trailing: Icon(Icons.thermostat, color: Colors.blueGrey),
-                      ),
-                      ListTile(
-                        title: Text('pH roztworu'),
-                        subtitle: Text('6.8'), // Replace with real-time data
-                        trailing: Icon(Icons.opacity, color: Colors.green),
-                      ),
-                      ListTile(
-                        title: Text('TDS (ppm)'),
-                        subtitle: Text('450'), // Replace with real-time data
-                        trailing: Icon(Icons.filter_tilt_shift, color: Colors.orange),
-                      ),
-                      ListTile(
-                        title: Text('Status czujników'),
-                        subtitle: Text('Wszystkie czujniki działają poprawnie'),
-                        trailing: Icon(Icons.check_circle, color: Colors.green),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Wykresy:',
-                style: GoogleFonts.lato(
-                  textStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              // Placeholder for charts
-              Container(
-                height: 200,
-                child: charts.TimeSeriesChart(
-                  _createSampleData(),
-                  animate: true,
-                  dateTimeFactory: charts.LocalDateTimeFactory(),
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'Podstawowe alarmy i powiadomienia:',
-                style: GoogleFonts.lato(
-                  textStyle: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20.0,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-              // Placeholder for alerts
-              Container(
-                padding: EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  color: Colors.red.withOpacity(0.1),
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: Text(
-                  'Uwaga: pH zbyt niskie! Wartość wynosi 5.6.',
+      body: Container(
+        color: Colors.white, // Ustawienie koloru tła na biały
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 20),
+                Text(
+                  'Widok danych w czasie rzeczywistym:',
                   style: GoogleFonts.lato(
                     textStyle: TextStyle(
-                      fontSize: 16.0,
-                      color: Colors.red,
                       fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.black,
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                // Placeholder for real-time data display
+                Card(
+                  color: Colors.white,
+                  elevation: 4,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: <Widget>[
+                        ListTile(
+                          title: Text('Temperatura wody'),
+                          subtitle: Text('22.5°C'), // Replace with real-time data
+                          trailing: Icon(Icons.thermostat, color: Colors.blueGrey),
+                        ),
+                        ListTile(
+                          title: Text('pH roztworu'),
+                          subtitle: Text('6.8'), // Replace with real-time data
+                          trailing: Icon(Icons.opacity, color: Colors.green),
+                        ),
+                        ListTile(
+                          title: Text('TDS (ppm)'),
+                          subtitle: Text('450'), // Replace with real-time data
+                          trailing: Icon(Icons.filter_tilt_shift, color: Colors.orange),
+                        ),
+                        ListTile(
+                          title: Text('Status czujników'),
+                          subtitle: Text('Wszystkie czujniki działają poprawnie'),
+                          trailing: Icon(Icons.check_circle, color: Colors.green),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Wykresy:',
+                  style: GoogleFonts.lato(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                // Placeholder for charts
+                Container(
+                  height: 200,
+                  child: charts.TimeSeriesChart(
+                    _createSampleData(),
+                    animate: true,
+                    dateTimeFactory: charts.LocalDateTimeFactory(),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Podstawowe alarmy i powiadomienia:',
+                  style: GoogleFonts.lato(
+                    textStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                // Placeholder for alerts
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.red.withOpacity(0.1),
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Text(
+                    'Uwaga: pH zbyt niskie! Wartość wynosi 5.6.',
+                    style: GoogleFonts.lato(
+                      textStyle: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.red,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
